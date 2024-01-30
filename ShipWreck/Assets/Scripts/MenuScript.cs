51e944f43b28b85251742df941cb95cea1f59526
Lucash5 <122258329+Lucash5@users.noreply.github.com>
2024-01-30 20:32:39 +0200

    asdawd
    a

---
+++ ShipWreck/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(loadgame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void loadgame()
+    {
+        scenemana
+    }
+}
